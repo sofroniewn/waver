0e1d03a9d81d2af0f22fc93e95f891808ec0cd91
Nicholas Sofroniew <sofroniewn@gmail.com>
2021-07-09 21:21:29 -0700

    improvements

--- waver/_dock_widget.py
+++ waver/_dock_widget.py
@@ -3,38 +3,48 @@ from napari_plugin_engine import napari_hook_implementation
 from .simulation import Simulation
 
 
+# ToDo, fix startup values for comboboxes
+# ToDo, expose spatial/ temporal downsample factors
+# ToDo, allow for "speed" to be inputed as an array somehow ....
+
 @magic_factory(call_button="run",
-              spacing={'min': 1e-6, 'max':1},
-              speed={'min': 1, 'max':1e5},
-              time_step={'min': 1e-10, 'max':1},
+              ndim={'min': 1, 'max':3, 'step': 1},
+              length={'min': .1, 'max':1e3, 'step': .1, 'label': 'length (mm)'},
+              spacing={'min': 10, 'max':10_000, 'step': 10, 'label': 'spacing (um)'},
+              speed={'min': 10, 'max':1e5, 'step': 1e1, 'label': 'speed (m/s)'},
+              time_step={'min': 10, 'max':1e6, 'step': 1e2, 'label': 'time step (ns)'},
+              duration={'min': 10, 'max':1e6, 'step': 10, 'label': 'duration (us)'},
+              frequency={'min': 1, 'max':1e4, 'step': 10, 'label': 'frequency (kHz)'},
 )
-def simulation(spacing: float=1e-3,
+def simulation(
+               ndim: int=2,
+               length: float=12.8,
+               spacing: float=100,
                speed: float=343, 
-               time_step: float=200e-9) -> 'napari.types.LayerDataTuple':
+               time_step: float=200,
+               duration: float=60,
+               frequency: float=500,               
+               ) -> 'napari.types.LayerDataTuple':
     """Run a single simulation.
     """
-    # Should be provided by magicgui, right now hard coded
-    size = (12.8e-3, 12.8e-3) # how to do tuple - could be 1D, 2D, 3D?
-    spacing=1e-5
-    speed=343
-    time_step=200e-9
-    period=5e-6
-    duration=60e-6
+    size = ndim * (length / 1e3,) # length always same in all dimensions
 
     # Create simulation
-    sim = Simulation(size=size, spacing=spacing, speed=speed, time_step=time_step)
+    sim = Simulation(size=size, spacing=spacing / 1e6, speed=speed, time_step=time_step / 1e9)
 
     # Add source
+    period = 1 / frequency / 1e3
     sim.add_source(location=(s/2 for s in size), period=period, ncycles=1)
 
     # Add detector grid
     sim.add_detector()
 
     # Run simulation
-    sim.run(duration=duration)
+    sim.run(duration=duration / 1e6)
 
     # Return simulation wave data
-    layer_dict = {'colormap': 'PiYG', 'contrast_limits':[-1.5, 1.5], 'name': 'wave'}
+    clim = max(sim.wave.max(), abs(sim.wave.min()))
+    layer_dict = {'colormap': 'PiYG', 'contrast_limits':[-clim, clim], 'name': 'wave'}
     return (sim.wave, layer_dict, 'Image')
 
 
